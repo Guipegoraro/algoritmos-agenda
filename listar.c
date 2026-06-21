ef750ab315b9614326467e18bff54ec0e9d681b5
Guilherme Cunha Pegoraro <97493836+Guipegoraro@users.noreply.github.com>
2026-06-21 05:29:24 -0300

    tira linha desnecessária

--- listar.c
+++ listar.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include "modelo_dados.h"
 
 int Listar() {
     printf("Contatos:\n");
 
-    for (int i=0; i < ULTIMO_ID_VALIDO; i++) {
-        int id = CONTATOS[ULTIMO_ID_VALIDO - 1].id;
+    for (int i = 0; i < ULTIMO_ID_VALIDO; i++) {
         printf("Id: %d, Nome: %s\n", CONTATOS[i].id, CONTATOS[i].nome);
     }
 
