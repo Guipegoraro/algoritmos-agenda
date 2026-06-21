ebc94a9f6a9d0eecc56766543505aa2370434d91
Guilherme Cunha Pegoraro <97493836+Guipegoraro@users.noreply.github.com>
2026-06-21 03:03:14 -0300

    Remove comentário desnecessário e organiza linhas

--- adicionar.c
+++ adicionar.c
@@ -5,21 +5,21 @@ int Adicionar()
 {
     Contato contatoNovo;
 
-    CONTATOS = (Contato *) realloc(CONTATOS, sizeof(Contato) * ULTIMO_ID_VALIDO);
-
+    CONTATOS = (Contato *)realloc(CONTATOS, sizeof(Contato) * ULTIMO_ID_VALIDO);
 
     ULTIMO_ID_VALIDO = ULTIMO_ID_VALIDO + 1;
-    
+
     contatoNovo.id = ULTIMO_ID_VALIDO;
-    
+
     printf("Preencha os dados do contato:\nNome: ");
     scanf("%s", &(contatoNovo.nome));
-    
+
     printf("Telefone: ");
     scanf("%s", &(contatoNovo.telefone));
-    
+
     int tipo = 0;
-    while (tipo != 1 && tipo != 2) {
+    while (tipo != 1 && tipo != 2)
+    {
         printf("Tipo de contato:\n1-Pessoal\n2-Trabalho: ");
         scanf("%d", &tipo);
         if (tipo != 1 && tipo != 2)
@@ -35,9 +35,9 @@ int Adicionar()
             contatoNovo.tipo = TRABALHO;
         }
     }
-    
+
     CONTATOS[ULTIMO_ID_VALIDO - 1] = contatoNovo;
-    //imprimir contato criado direto da lista de contatos
+    
     printf("Contato cadastrado!\n");
     int id = CONTATOS[ULTIMO_ID_VALIDO - 1].id;
     char *nome = CONTATOS[ULTIMO_ID_VALIDO - 1].nome;
