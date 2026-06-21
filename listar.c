07bfc8cb3c6d5bcff556ccdb517dc45fc054ad2c
Guilherme Cunha Pegoraro <97493836+Guipegoraro@users.noreply.github.com>
2026-06-21 03:03:46 -0300

    Remove linha desnecessária

--- listar.c
+++ listar.c
@@ -5,7 +5,6 @@ int Listar() {
 
     for (int i=0; i < ULTIMO_ID_VALIDO; i++) {
         int id = CONTATOS[ULTIMO_ID_VALIDO - 1].id;
-        char *nome = CONTATOS[ULTIMO_ID_VALIDO - 1].nome;
         printf("Id: %d, Nome: %s\n", CONTATOS[i].id, CONTATOS[i].nome);
     }
 
